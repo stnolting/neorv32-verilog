9927691bec11e2e951b3e2433d7b3989cebaf97d
stnolting <stnolting@gmail.com>
2022-08-27 19:12:34 +0200

    Update neorv32_verilog_wrapper.vhd

--- src/neorv32_verilog_wrapper.vhd
+++ src/neorv32_verilog_wrapper.vhd
@@ -1,41 +1,3 @@
--- #################################################################################################
--- # << NEORV32 - Wrapper for GHDL-based to-Verilog (netlist) conversion >>                        #
--- # ********************************************************************************************* #
--- # This example uses a minimal NEORV32 Processor/CPU configuration that runs the default         #
--- # bootloader. You can add additional NEORV32 interface ports if required. Use the processor     #
--- # generics to configure the system according to your needs (*before* converting to Verilog).    #
--- # ********************************************************************************************* #
--- # BSD 3-Clause License                                                                          #
--- #                                                                                               #
--- # Copyright (c) 2022, Stephan Nolting. All rights reserved.                                     #
--- #                                                                                               #
--- # Redistribution and use in source and binary forms, with or without modification, are          #
--- # permitted provided that the following conditions are met:                                     #
--- #                                                                                               #
--- # 1. Redistributions of source code must retain the above copyright notice, this list of        #
--- #    conditions and the following disclaimer.                                                   #
--- #                                                                                               #
--- # 2. Redistributions in binary form must reproduce the above copyright notice, this list of     #
--- #    conditions and the following disclaimer in the documentation and/or other materials        #
--- #    provided with the distribution.                                                            #
--- #                                                                                               #
--- # 3. Neither the name of the copyright holder nor the names of its contributors may be used to  #
--- #    endorse or promote products derived from this software without specific prior written      #
--- #    permission.                                                                                #
--- #                                                                                               #
--- # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS   #
--- # OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF               #
--- # MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE    #
--- # COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,     #
--- # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE #
--- # GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED    #
--- # AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     #
--- # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED  #
--- # OF THE POSSIBILITY OF SUCH DAMAGE.                                                            #
--- # ********************************************************************************************* #
--- # The NEORV32 Processor - https://github.com/stnolting/neorv32-verilog      (c) Stephan Nolting #
--- #################################################################################################
-
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -62,30 +24,30 @@ begin
   neorv32_top_inst: neorv32_top
   generic map (
     -- General --
-    CLOCK_FREQUENCY              => 100000000,   -- clock frequency of clk_i in Hz
-    INT_BOOTLOADER_EN            => true,        -- boot configuration: true = boot explicit bootloader; false = boot from int/ext (I)MEM
+    CLOCK_FREQUENCY              => 100000000, -- clock frequency of clk_i in Hz
+    INT_BOOTLOADER_EN            => true,      -- boot configuration: true = boot explicit bootloader; false = boot from int/ext (I)MEM
 
     -- RISC-V CPU Extensions --
-    CPU_EXTENSION_RISCV_C        => true,        -- implement compressed extension?
-    CPU_EXTENSION_RISCV_M        => true,        -- implement mul/div extension?
-    CPU_EXTENSION_RISCV_Zicsr    => true,        -- implement CSR system?
-    CPU_EXTENSION_RISCV_Zicntr   => true,        -- implement base counters?
-    CPU_EXTENSION_RISCV_Zifencei => true,        -- implement instruction stream sync.?
+    CPU_EXTENSION_RISCV_C        => true,      -- implement compressed extension?
+    CPU_EXTENSION_RISCV_M        => true,      -- implement mul/div extension?
+    CPU_EXTENSION_RISCV_Zicsr    => true,      -- implement CSR system?
+    CPU_EXTENSION_RISCV_Zicntr   => true,      -- implement base counters?
+    CPU_EXTENSION_RISCV_Zifencei => true,      -- implement instruction stream sync.?
 
     -- Internal Instruction memory (IMEM) --
-    MEM_INT_IMEM_EN              => true,        -- implement processor-internal instruction memory
-    MEM_INT_IMEM_SIZE            => 16*1024,     -- size of processor-internal instruction memory in bytes
+    MEM_INT_IMEM_EN              => true,      -- implement processor-internal instruction memory
+    MEM_INT_IMEM_SIZE            => 16*1024,   -- size of processor-internal instruction memory in bytes
 
     -- Internal Data memory (DMEM) --
-    MEM_INT_DMEM_EN              => true,        -- implement processor-internal data memory
-    MEM_INT_DMEM_SIZE            => 8*1024,      -- size of processor-internal data memory in bytes
+    MEM_INT_DMEM_EN              => true,      -- implement processor-internal data memory
+    MEM_INT_DMEM_SIZE            => 8*1024,    -- size of processor-internal data memory in bytes
 
     -- Processor peripherals --
-    IO_GPIO_EN                   => true,        -- implement general purpose input/output port unit (GPIO)?
-    IO_MTIME_EN                  => true,        -- implement machine system timer (MTIME)?
-    IO_UART0_EN                  => true,        -- implement primary universal asynchronous receiver/transmitter (UART0)?
-    IO_UART0_RX_FIFO             => 1,           -- RX fifo depth, has to be a power of two, min 1
-    IO_UART0_TX_FIFO             => 1            -- TX fifo depth, has to be a power of two, min 1
+    IO_GPIO_EN                   => true,      -- implement general purpose input/output port unit (GPIO)?
+    IO_MTIME_EN                  => true,      -- implement machine system timer (MTIME)?
+    IO_UART0_EN                  => true,      -- implement primary universal asynchronous receiver/transmitter (UART0)?
+    IO_UART0_RX_FIFO             => 1,         -- RX fifo depth, has to be a power of two, min 1
+    IO_UART0_TX_FIFO             => 1          -- TX fifo depth, has to be a power of two, min 1
   )
   port map (
     -- Global control --
